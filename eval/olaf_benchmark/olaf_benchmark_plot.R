7d92e7368fca2ea6f2090d682d23f5e71e99f031
Joren Six <joren@0110.be>
2023-03-13 16:38:48 +0100

    joss version

--- eval/olaf_benchmark/olaf_benchmark_plot.R
+++ eval/olaf_benchmark/olaf_benchmark_plot.R
@@ -29,6 +29,10 @@ a <- ggplot(bm, aes(bm$seconds, bm$query_real_time)) +
                     breaks = scales::trans_breaks("log10", function(x) 10^x),
                     labels = scales::trans_format("log10", scales::math_format(10^.x))
                  ) +
+          scale_y_log10(
+                    breaks = scales::trans_breaks("log10", function(x) 10^x),
+                     labels = scales::trans_format("log10", scales::math_format(10^.x))
+                 ) +
            theme_bw();
 a <- a + annotation_logticks();
 ggsave(file="olaf_benchmark_query.svg", plot=a, width=10, height=8)
