59a71498f063dd69608cb0c68a171950d51648fe
Joren Six <joren@0110.be>
2023-03-13 17:21:08 +0100

    docs script

--- docs/update_docs.rb
+++ docs/update_docs.rb
@@ -10,11 +10,9 @@ system("doxygen")
 Dir.mktmpdir do |d|
 	system("cd '#{d}' && git clone git@github.com:JorenSix/Olaf.git")
 	system("cd '#{d}/Olaf' && git checkout gh-pages")
-	system("cp -r #{html_files} #{d}/Olaf")
+	system("cp -r #{html_files} '#{d}/Olaf' ")
 	system("cd '#{d}/Olaf' && git add .")
 	system("cd '#{d}/Olaf' && git commit -m docs")
 	system("cd '#{d}/Olaf' && git push origin gh-pages")
 end
-
-
 
