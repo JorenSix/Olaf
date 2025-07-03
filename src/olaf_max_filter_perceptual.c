4a435105f0c855e128be3a0f39b749011b9fe4d8
Joren Six <joren.six@ugent.be>
2025-07-03 14:12:20 +0200

    chore:update-header

--- src/olaf_max_filter_perceptual.c
+++ src/olaf_max_filter_perceptual.c
@@ -1,5 +1,5 @@
 // Olaf: Overly Lightweight Acoustic Fingerprinting
-// Copyright (C) 2019-2023  Joren Six
+// Copyright (C) 2019-2025  Joren Six
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
